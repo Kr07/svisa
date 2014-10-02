da43db2fa82eb1a81a98bec0bbd49502fbb9c6df
Roberto <Roberto@VERS3110MJPTPM7.ac.sep.gob.mx>
2014-10-02 13:19:40 -0500

    actualizacion de menu

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,33 +1,33 @@
-<div class="navbar navbar-inverse">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#">Company Info</a>
-    </div>
-    <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home Page</a></li>
-            <li><a href="#">Contacts</a></li>
-            <li><a href="#">About</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Social</a></li>
-        </ul>
-    </div>
-</div>
-
 <div class="container">
-    <form id="registrationForm" method="post" class="form-horizontal" action="...">
     <div class="row">
        <div class="col-lg-12"><p>LISTA DE VERIFICACIÓN DE LA CALIDAD DE LA INSTALACIÓN DE LA SOLUCIÓN DE AULA DEL PROGRAMA DE INCLUSIÓN Y ALFABETIZACIÓN DIGITAL</p></div>
        <div class="col-lg-12"><br />La presente Lista de Verificación con fundamento en el articulo 108 del Reglamento de la Ley de Adquisiciones, Arrendamientos y Servicios del Sector Público, con la finalidad de verificar la calidad de la prestación del servicio de instalación de los bienes que componen ala solución de aulas en las escuelas.</div>
        <div class="col-lg-12"><br /><p>VERIFICACIÓN DE CALIDAD EN SITIO DE LA INSTALACIÓN DE LA SOLUCIÓN DE AULA&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fecha:01/10/2014</p></div>  
     </div>
-    
-    <br /><br />        
+
+</div>
+
+<div class="container">  
+   
+<form action="DatosGenerales/mostrar_DatosGenerales">
+    <br /><br />
+        <div class="row">
+          <div class="col-lg-6"> <input type="text" class="form-control" name="CCT" placeholder="Escribe el CCT" required /></div>
+          <div class="col-lg-6"><button type="submit" name="submit" class="btn btn-success">Buscar CCT</button></div>
+       </div>
+</form>
+
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#defaultForm').bootstrapValidator();
+    });
+</script>
+
+
+<div class="container">
+   <br /><br />        
     <div class="row">
       <div class="col-md-6 col-md-push-6">ESCUELA CON CALIDAD REGULAR <img src="<?php echo base_url('assets/img/img1.jpg') ?>"class="img-circle"></div>
       <div class="col-md-6 col-md-pull-6">
@@ -54,15 +54,11 @@
              <div class="col-lg-12">No. de Docentes:</div>
              <div class="col-lg-12"><input type="text" class="form-control"></div>
              <div class="col-lg-12">Correo Electronico:</div>
-             <div class="col-lg-12">
-             <div class="form-group"><input type="text" class="form-control" name="email" placeholder="Email address2" /></div>
-
-             </div>
+             <div class="col-lg-12"><input type="text" class="form-control"/></div>
              <div class="col-lg-12"><br />Numero de Aulas: 
-                <label class="radio-inline">
-                   <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 1</label>
-                   <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 2</label>
-                   <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> 3</label>
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 1</label>
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 2</label>
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> 3</label>
              </div>
             </div>
 
@@ -72,8 +68,4 @@
      <div class="row">
      <div class="col-lg-12"><p style="text-align:center;"><br /><button type="submit" name="submit" class="btn btn-success">Comenzar Encuesta</button></p></div>
      </div>
-    </form>
-
-
-
 </div>
