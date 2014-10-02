1088b6614c9c5dfeb89fca755d03c927ea43e741
lenovo <lenovo@lenovo-THINK>
2014-10-02 16:51:42 -0500

    Se agrego la actualizacion del cct

--- application/controllers/DatosGenerales.php
+++ application/controllers/DatosGenerales.php
@@ -19,14 +19,46 @@
             $this->load->library('form_validation');
             
             $cct = $this->input->post('cve_cct');
-            $entidad= $this->input->post('entidad');
-            if($entidad == null)
-                $entidad = 9;
+            $entidad= $this->session->userdata('id_entidad');//$this->input->post('entidad');
+            
+            /*if($entidad == null)
+                $entidad = 9;*/
             $this->load->model('c_CCT');
             $datosGenerales = $this->c_CCT->busca_cCCT($entidad, $cct);
 
             $this->load->view('home_view', $datosGenerales);
         }
+        function actualiza_cCCT(){
+		//$this->load->helper('form');
+		/*$this->load->library('form_validation');
+		$this->form_validation->set_rules('nombre', 'Nombre', 'required|min_length[3]');
+		$this->form_validation->set_rules('sueldo', 'Sueldo', 'required|numeric');*/
+			
+		//if($this->form_validation->run() === true){
+			//Si la validación es correcta, cogemos los datos de la variable POST
+			//y los enviamos al modelo
+                //$tel_cct, $matricula, $no_docente, $e_mail, $num_aulas
+                $id_cct = $this->input->post('id_cct');
+		$tel_cct = $this->input->post('tel_cct');
+		$matricula = $this->input->post('matricula');
+                $no_docente = $this->input->post('no_docente');
+		$e_mail = $this->input->post('e_mail');
+                $num_aulas = $this->input->post('num_aulas');
+                        
+                $data = array(
+                    'tel_cct'   => $tel_cct,
+                    'matricula' => $matricula,
+                    'no_docente'=> $no_docente,
+                    'e_mail'    => $e_mail,
+                    'num_aulas' => $num_aulas
+		);
+		
+		$this->load->model('empleados_model');
+		$this->empleados_model->actualiza_cCCT($id_cct, $data);
+		//}
+			
+		$this->load->view("home_view");
+	}
     }
 
 ?>
