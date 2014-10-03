d59d0567578f5aba084e4f7d1dfae7e58a3509a7
lenovo <lenovo@lenovo-THINK>
2014-10-03 13:44:31 -0500

    Para visualizar encuesta

--- application/models/Encuesta.php
+++ application/models/Encuesta.php
@@ -15,26 +15,47 @@
         function __construct(){
             $this -> load -> database();
         }
-        function obtener_cSecciones($no_seccion){
-            $query = $this -> db -> select('')
-                                 -> from('c_secciones')
-                                 -> where('num_encuesta', $no_seccion)
-                                 -> get();
+        function obtener_cReactivos($no_seccion){
 
-            return $query -> result_array();
-        }
-        function obtener_cReactivos(){
-            $lstSecciones = array {
-                'id' =>
-                
-            };
-            obtener_cSecciones
-            $query = $this -> db -> select('')
-                                 -> from('c_reactivos')
-                                 -> where('')
+            $query = $this -> db -> select('c_seccion_encuesta.id_seccion_reactivo, c_seccion_encuesta.num_encuesta, c_seccion_encuesta_nom_seccion, '
+                    . 'c_seccion_encuesta.status, c_seccion_encuesta.alcance,c_reactivos.id_reactivos, c_reactivos.id_padre, c_reactivos.tipo_reactivo,'
+                    . 'c_reactivos.reactivo, c_reactivos.ponderacion ')
+                    -> from('c_reactivos')
+                    ->join('c_seccion_encuesta','c_reactivos.id_seccion_reactivo = c_seccion_encuesta.id_seccion_reactivos', 'inner')
+                    -> where('c_seccion_encuesta.num_encuesta', $no_seccion)
+                    -> where('status', 1)
                     -> get();
-            
-            return $query -> result_array();
+            $dept = array();
+            if ($query->num_rows() > 0){
+                $row = $query->row_array(); 
+                
+                while($row = mysql_fetch_array($result)){
+                    $dep[]['id_seccion_reactivo'] = $row['c_seccion_encuesta.id_seccion_reactivo'];
+                    $dep[]['num_encuesta'] = $row['c_seccion_encuesta.num_encuesta'];
+                    $dep[]['nom_seccion'] = $row['c_seccion_encuesta.nom_seccion'];
+                    $dep[]['status'] = $row['c_seccion_encuesta.status'];
+                    $dep[]['alcance'] = $row['c_seccion_encuesta.alcance'];
+                    $dep[]['id_reactivos'] = $row['c_reactivos.id_reactivos'];
+                    $dep[]['id_padre'] = $row['c_reactivos.id_padre'];
+                    $dep[]['reactivo'] = $row['c_reactivos.reactivo'];
+                    $dep[]['ponderacion']= $row['c_reactivos.ponderacion'];
+                    if($row['c_reactivos.tipo_reactivo']== 2){
+                        $query2 = $this -> db -> select('id_opciones, id_reactivo, id_seccion_reactivo, respuesta, inciso')
+                                        -> from('c_opciones')
+                                        -> where ('id_reactivo', $row['c_reactivos.id_reactivos'])
+                                        -> where ('id_seccion_reactivo', $row['c_seccion_encuesta.id_seccion_reactivo'])
+                                        ->get();
+                        $dep[]['tipo_reactivo'][] = $query2->row_array(); 
+                    }
+                    else{
+                        $dep[]['tipo_reactivo'] = $row['c_reactivos.tipo_reactivo'];
+                    }
+                }
+                return $dept;
+            }
+            else{
+                return false;
+            }
         }
         
         //put your code here
