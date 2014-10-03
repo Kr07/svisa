039270b2bb05ee60890c759e2a64d8d38e168bbe
lenovo <lenovo@lenovo-THINK>
2014-10-03 18:35:37 -0500

    Insertar y actualizar la tabla info_verificacion

---
+++ application/models/info_Verificacion.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of info_Verificacion
+ *
+ * @author lenovo
+ */
+    class info_Verificacion extends CI_Model {
+        function __construct(){
+            $this -> load -> database();
+        }
+        function insertar_infVerificacion($usuario, $cct, $semaforo){
+            $data = array(
+                'id_usuario' => $usuario,
+                'id_cct' => $cct,
+                'fch_verificacion' => now(),
+                'semaforo' => $semaforo                
+            );
+            $this->db->insert('ver_aula', $data); 
+            
+        }
+        function actualizar_infVerificacion($id_verficacion,$usuario, $cct, $semaforo){
+            $data = array(
+                'id_usuario' => $usuario,
+                'id_cct' => $cct,
+                'fch_verificacion' => now(),
+                'semaforo' => $semaforo                
+            );
+            $this->db->where('id_verificacion', $id_verficacion);
+            $this->db->update('info_verificacion', $data); 
+            
+        }
+        //put your code here
+    }
