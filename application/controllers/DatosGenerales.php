8f9db701e339e4e2c89dcc75e56e2ef52b1e20cd
lenovo <lenovo@lenovo-THINK>
2014-10-02 19:08:08 -0500

    Se agrego la actualizacion del cct

--- application/controllers/DatosGenerales.php
+++ application/controllers/DatosGenerales.php
@@ -18,14 +18,14 @@
             $this->load->helper('form');
             $this->load->library('form_validation');
             
-            $cct = $this->input->post('cve_cct');
+            $cct = $this->input->post('cct');
             $entidad= $this->session->userdata('id_entidad');//$this->input->post('entidad');
             
             /*if($entidad == null)
                 $entidad = 9;*/
             $this->load->model('c_CCT');
             $datosGenerales = $this->c_CCT->busca_cCCT($entidad, $cct);
-
+            
             $this->load->view('home_view', $datosGenerales);
         }
         function actualiza_cCCT(){
