eaf1fdec0c4dd8f759326c5c1371c95f452aee30
Roberto <Roberto@VERS3110MJPTPM7.ac.sep.gob.mx>
2014-10-03 11:26:56 -0500

    INICIO DE ENCUESTA

---
+++ application/views/encuesta.php
@@ -0,0 +1,60 @@
+<div class="container">
+	<div class="box_center">
+    	<div class="box_br">
+  	 <form id="registrationForm" method="post" class="form-horizontal" action="...">
+	  <div class="row">
+	   <div class="col-lg-12"><p class="title_grey" align="center">LISTA DE VERIFICACIÓN DE LA CALIDAD DE LA INSTALACIÓN DE LA SOLUCIÓN <br>
+	     DE AULA DEL PROGRAMA DE INCLUSIÓN Y ALFABETIZACIÓN DIGITAL</p></div>
+	   <div class="col-lg-12"><br /><p class="title_grey_02">SOBRE LA INSTALACIÓN</p></div>
+           <div class="col-lg-12"><p class="title_grey_03">Aula Número 1: <b>Quinto A</b></p></div>	
+          </div>	
+      <div class="row">
+      <br />    
+      <div class="col-md-12">
+            <div class="row">
+             
+             <div class="col-lg-12"><h4 class="title_gray_quiz">¿Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.? </h4></div>
+             <div class="col-lg-12" style="padding-left:24px;">
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><h4 class="title_red_radiob">SI</H4></label>
+				<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"><h4 class="title_red_radiob">NO</H4></label>
+			 <hr>
+             </div>
+                          <div class="col-lg-12"><h4 class="title_gray_quiz">¿Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.? </h4></div>
+             <div class="col-lg-12" style="padding-left:24px;">
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><h4 class="title_red_radiob">SI</H4></label>
+				<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"><h4 class="title_red_radiob">NO</H4></label>
+			 <hr>
+             </div>
+                          <div class="col-lg-12"><h4 class="title_gray_quiz">¿Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.? </h4></div>
+             <div class="col-lg-12" style="padding-left:24px;">
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><h4 class="title_red_radiob">SI</H4></label>
+				<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"><h4 class="title_red_radiob">NO</H4></label>
+			 <hr>
+             </div>
+                          <div class="col-lg-12"><h4 class="title_gray_quiz">¿Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.? </h4></div>
+             <div class="col-lg-12" style="padding-left:24px;">
+                <label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"><h4 class="title_red_radiob">SI</H4></label>
+				<label class="radio-inline"><input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"><h4 class="title_red_radiob">NO</H4></label>
+			 <hr>
+             </div>
+             
+             <div class="col-lg-12"><h4 class="title_gray_quiz">Comentarios:</h4></div>
+             <div class="col-lg-12">
+               <textarea class="form-control"></textarea>
+             </div>
+          </div>
+           
+        </div>
+
+      	</div>
+    <div class="row">
+    	 <div class="col-lg-12"><p class="title_grey_tx">La presente Lista de Verificación con fundamento en el articulo 108 del Reglamento de la Ley de Adquisiciones, Arrendamientos y Servicios del Sector Público, con la finalidad de verificar la calidad de la prestación del servicio de instalación de los bienes que componen ala solución de aulas en las escuelas.</p></div>
+         <div class="col-lg-12"><p style="text-align:center;">
+           <button class="btn btn-lg btn-primary btn-block" type="submit">Continuar</button>
+         </p>
+         </div>
+   </div>
+  
+    </form>
+        </div>
+	</div>   
