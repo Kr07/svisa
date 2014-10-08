8ca57a3fdbd957b6bc0533b094e712a60adf1725
Roberto <Roberto@VERS3110MJPTPM7.ac.sep.gob.mx>
2014-10-08 13:40:04 -0500

    actualizacion de la encuesta

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -87,15 +87,15 @@ $(document).ready(function(){
              <div class="col-lg-12"><input type="text" class="form-control" id="nom_localidad" name="nom_localidad" disabled></div>
 <!--             <div class="col-lg-12"><h4 class="title_red_datos">Clave CCT:</h4></div>
              <div class="col-lg-12"><input type="text" class="form-control" id="cve_cct" name="cve_cct" disabled></div>-->
-             <div class="col-lg-12"><h4 class="title_red_datos">Teléfono:</h4></div>
+             <div class="col-lg-12"><h4 class="title_red_datos">Teléfono de la Escuela:</h4></div>
              <div class="col-lg-12"><input type="text" class="form-control" id="tel_cct" name="tel_cct" required></div>
-             <div class="col-lg-12"><h4 class="title_red_datos">Matricula Alumnos:</h4></div>
+             <div class="col-lg-12"><h4 class="title_red_datos">Matricula de Alumnos (Todos los grados):</h4></div>
              <div class="col-lg-12"><input type="text" class="form-control" id="matricula" name="matricula" required></div>
-             <div class="col-lg-12"><h4 class="title_red_datos">No. de Docentes:</h4></div>
+             <div class="col-lg-12"><h4 class="title_red_datos">No. de Docentes (Adscritos ala escuela):</h4></div>
              <div class="col-lg-12"><input type="text" class="form-control" id="no_docente" name="no_docente" required></div>
-             <div class="col-lg-12"><h4 class="title_red_datos">Correo Electronico:</h4></div>
+             <div class="col-lg-12"><h4 class="title_red_datos">Correo Electronico del director:</h4></div>
              <div class="col-lg-12"><input type="text" class="form-control" id="e_mail" name="e_mail" placeholder="Correo Electronico" required/></div>
-             <div class="col-lg-12"><h4 class="title_red_datos02">Seleccionar el número de aulas</h4></div>
+             <div class="col-lg-12"><h4 class="title_red_datos02">Seleccionar el número de aulas con soluciones instaladas</h4></div>
              <div class="col-lg-12">
                    <label class="radio-inline"><input type="radio" name="num_aulas" id="inlineRadio1" value="1" required> 1</label>
                    <label class="radio-inline"><input type="radio" name="num_aulas" id="inlineRadio2" value="2" required> 2</label>
